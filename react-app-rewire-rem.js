fecf76efbbc5ce79d332594fa60fdc848fa77f9b
linzhipeng <zhipeng.lin@ifchange.com>
2018-07-16 10:18:27 +0800

    添加了一些文档

--- react-app-rewire-rem.js
+++ react-app-rewire-rem.js
@@ -3,10 +3,17 @@ const { getLoader } = require("react-app-rewired");
 const createRewireRem=(loaderOptions={})=>{
     return function(config, env) {
         const cssRules=getLoader(config.module.rules,rule => String(rule.test) === String(/\.css$/));
-        cssRules.use.push({
-            loader:require.resolve("px2rem-loader"),
-            options:loaderOptions
-        });
+        if (env === "production") {
+            cssRules.loader.push({
+                loader:require.resolve("px2rem-loader"),
+                options:loaderOptions
+            });
+        }else{
+            cssRules.use.push({
+                loader:require.resolve("px2rem-loader"),
+                options:loaderOptions
+            });
+        }
         return config;
     };
 };
